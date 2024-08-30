f3e4a96d9ecd44feb303f6d84cdde9c2750f8ff2
kaneko4257 <168108231+kaneko4257@users.noreply.github.com>
2024-08-30 10:58:50 +0900

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,3 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('自己紹介サイトへようこそ！');
 });
 
-/*
-Background Gradients From -- 
-https://uigradients.com
-*/
-
-const CLASSNAME = "-visible";
-const TIMEOUT = 1500;
-const $target = $(".title");
-
